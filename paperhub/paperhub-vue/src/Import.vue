43199c096096be94022c631b4fd98f1d71a2ea7c
bun133 <bun133.work@gmail.com>
2022-02-01 16:55:30 +0900

    いいかんじにImport

--- paperhub/paperhub-vue/src/Import.vue
+++ paperhub/paperhub-vue/src/Import.vue
@@ -5,9 +5,15 @@
       <v-container>
         <h1>Import Server</h1>
         <h2>Server File</h2>
-        <v-file-input v-model="file" accept=".jar"></v-file-input>
+        <v-file-input v-model="file" accept=".jar" @change="autoCompleteFields"></v-file-input>
         <h2>Profile Name</h2>
         <v-text-field v-model="profileName"></v-text-field>
+        <h2>Minecraft Version</h2>
+        <MinecraftVersionSelector v-model="minecraftVersion"></MinecraftVersionSelector>
+        <h2>Paper Version</h2>
+        <PaperVersionSelector v-model="paperVersion" :minecraft-version="minecraftVersion"></PaperVersionSelector>
+        <h2>Execute Arguments</h2>
+        <v-text-field v-model="executeArguments"></v-text-field>
         <v-btn color="primary" @click="importServer(file)" :disabled="!this.isAllFieldsValid">Import Server</v-btn>
       </v-container>
     </v-main>
@@ -16,35 +22,59 @@
 
 <script>
 import HangBar from "@/components/HangBar";
+import MinecraftVersionSelector from "@/components/MinecraftVersionSelector";
+import PaperVersionSelector from "@/components/PaperVersionSelector";
+
+const path = require("path");
 
 export default {
   name: "import",
-  components: {HangBar},
+  components: {PaperVersionSelector, MinecraftVersionSelector, HangBar},
   data() {
     return {
       file: "",
       profileName: "",
+      minecraftVersion: "",
+      paperVersion: "",
+      executeArguments: "",
     }
   },
   methods: {
     importServer(file) {
       console.log("importServer", file);
       window.ipcRenderer.send("toMain@AddServer", {
-        title: "Paper 1.18.2",
-        folderPath: "",
-        executablePath: "",
-        minecraftVersion: "1.18.2",
-        paperVersion: "75",
-        executeArguments: ""
+        title: this.profileName,
+        folderPath: path.dirname(file.path),
+        executablePath: file.path,
+        minecraftVersion: this.minecraftVersion,
+        paperVersion: this.paperVersion,
+        executeArguments: this.executeArguments
       });
+    },
+    autoCompleteFields() {
+      const name = this.file.name;
+      // The File Name is this pattern: "paper-<minecraftVersion>-<paperVersion>.jar"
+      // So we can auto complete the Minecraft Version and Paper Version
+      this.minecraftVersion = name.split("-")[1];
+      this.paperVersion = name.split("-")[2].split(".")[0];
+      // TODO 表示に反映されない
     }
   },
   computed: {
     isFileValid() {
       return this.file instanceof File;
     },
+    isProfileNameValid() {
+      return this.profileName.length > 0;
+    },
+    isMinecraftVersionValid() {
+      return this.minecraftVersion.length > 0;
+    },
+    isPaperVersionValid() {
+      return this.paperVersion.toString().length > 0;
+    },
     isAllFieldsValid() {
-      return this.isFileValid;
+      return this.isFileValid && this.isProfileNameValid && this.isMinecraftVersionValid && this.isPaperVersionValid;
     }
   }
 }
